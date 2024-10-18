f2dd70e748f0843fbaf134fb4c09a67c55b82628
Sanaz.darbandi <mahta17rw@gmail.com>
2024-10-18 18:39:21 +0330

    soal1

---
+++ Taklif1DS/Taklif1DS/Program.cs
@@ -0,0 +1,25 @@
+﻿//question1
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int a = 10;
+        int b = 3;
+
+        int result = Divide(a, b);
+        Console.WriteLine($"The result of {a} / {b} is: {result}");
+    }
+
+    public static int Divide(int a, int b)
+    {
+
+        if (a < b)
+        {
+            return 0;
+        }
+
+        return 1 + Divide(a - b, b);
+    }
+}
